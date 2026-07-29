f47dd685888f870e339e131ca601690438d677a9
Jonas <jonasvm6@hotmail.com>
2026-07-29 18:54:41 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Planora',
+    description: 'A planning system for internal task administration',
+    image: '/images/project-name.png',
+    repoUrl: 'https://github.com/Jnbp24/ProjectPlanora',
+    tags: ['C#', '.NET', 'Entity Framework Core', 'SQL Server', "Javascript", "HTML & CSS"],
+  },
+    {
+    id: 2,
+    title: 'Eaat Delivery App',
+    description: 'A Wolt-inspired food delivery platform built with a microservices architecture, including restaurant and courier services connected via RabbitMQ - App focused on System integration, purely working through command prompt',
+    image: '/images/eaat.png',
+    repoUrl: 'https://github.com/Jnbp24/EksamenSysInt',
+    tags: ['C#', '.NET', 'RabbitMQ', 'Microservices'],
+    },
+    {
+    id: 3,
+    title: 'Portfolio site',
+    description: 'This portfolio site itself',
+    image: '/images/portfolio.png',
+    repoUrl: 'https://github.com/Jnbp24/YOUR-REPO-NAME',
+    tags: ['React', 'Vite', 'React Router'],
+    }
+]
+
+export default projects
